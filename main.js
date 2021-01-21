482cb7300d70322ebabb6c91cefe278b67bbf376
StefanieSeskin <stefanieaseskin@gmail.com>
2021-01-21 17:58:21 -0500

    v2

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let products = items.results;
 let titles = products.map(function (item) {
   return `<div class = "products">
     <div><img class = "product-image" src="${item.Images[0].url_fullxfull}"></div>
-
+    <div class = "item-name">${item.title}</div>
     <div class = "shop-name">${item.Shop.shop_name}</div>
     <div class = "stars">(${item.views})</div>
-    <div class ="item-price">${item.price}</div>
+    <div class ="item-price">$${item.price}</div>
   </div>`;
 });
 document.querySelector("#grid").innerHTML = titles.join("");
