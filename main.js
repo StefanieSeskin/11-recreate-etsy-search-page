249be1befac39da797809927cf26df3f015395f1
StefanieSeskin <stefanieaseskin@gmail.com>
2021-01-22 17:40:13 -0500

    ll

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 let products = items.results;
 let titles = products.map(function (item) {
-  return `<div class = "products">
-    <div><img class = "product-image" src="${item.Images[0].url_fullxfull}"></div>
-    <div class = "item-name">${item.title}</div>
-    <div class = "shop-name">${item.Shop.shop_name}</div>
-    <div class = "stars">(${item.views})</div>
-    <div class ="item-price">$${item.price}</div>
-  </div>`;
+  return `<div class="products">
+  <a class="top-links" href="#">
+    <div><img class="product-image" src="${item.Images[0].url_fullxfull}"></div>
+    <div class="item-name">${item.title}</div>
+    <div class="shop-name">${item.Shop.shop_name}</div>
+    <div class="stars">*****(${item.views})</div>
+    <div class="item-price">$${item.price} </div>
+    <div class="placeholder-text">free shipping ...</div>
+  </a>
+</div>`;
 });
 document.querySelector("#grid").innerHTML = titles.join("");
 
@@ -18,4 +21,4 @@ document.querySelector("#grid").innerHTML = titles.join("");
 //     <strong>${item.price}</strong>
 //     </div>`
 
-/* <div class = "item-name">${item.title}</div> */
+/* <div class="item-name">${item.title}</div> */
